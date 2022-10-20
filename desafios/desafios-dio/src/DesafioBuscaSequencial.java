c10fd1b51a223222847c263ee061de03c2ec02aa
andersonsolon <anderson.solon@gmail.com>
2022-10-20 09:06:27 -0300

    update the folder desafios

---
+++ desafios/desafios-dio/src/DesafioBuscaSequencial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DesafioBuscaSequencial {
+  public static void main(String[] args) {
+
+    try (Scanner leitor = new Scanner(System.in)) {
+      int[] elementos = { 64, 137, -16, 43, 67, 81, -90, 212, 10, 75 };
+
+      int num = leitor.nextInt();
+
+      int i;
+      for (i = 0; i < elementos.length; i++) {
+        if (elementos[i] == num) {
+          System.out.println("Achei " + num + " na posicao " + i);
+          break;
+        }
+      }
+      if (i == elementos.length) {
+        System.out.println("Numero " + num + " nao encontrado!");
+      }
+    }
+
+  }
+}
---
+++ desafios/desafios-dio/src/DesafioNumerosIguais.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class DesafioNumerosIguais {
+  public static void main(String[] args) {
+    try (Scanner leitor = new Scanner(System.in)) {
+      int A = leitor.nextInt();
+      int B = leitor.nextInt();
+
+      if (A == B) {
+        System.out.println("Sao iguais!");
+      } else {
+        System.out.println("Nao sao iguais!");
+      }
+    }
+  }
+}
